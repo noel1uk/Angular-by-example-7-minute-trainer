aef166444e52fabf618243a96fc9a91e0550d286
Noel Vock <noel1uk@gmail.com>
2018-09-20 22:00:40 +0100

    feat: add exercises that update on page

--- src/app/workout-runner/workout-runner.module.ts
+++ src/app/workout-runner/workout-runner.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { WorkoutRunnerComponent } from './workout-runner.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: []
+  declarations: [WorkoutRunnerComponent],
+  exports: [WorkoutRunnerComponent]
 })
 export class WorkoutRunnerModule { 
   
